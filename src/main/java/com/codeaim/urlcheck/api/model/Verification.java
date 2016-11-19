89f3442d684186226568735478c0e4a720540c6a
GDownes <gdownes@gmail.com>
2016-11-19 22:02:12 +0000

    Adding send verification email endpoint

--- src/main/java/com/codeaim/urlcheck/api/model/Verification.java
+++ src/main/java/com/codeaim/urlcheck/api/model/Verification.java
@@ -1,5 +1,12 @@
 package com.codeaim.urlcheck.api.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@Accessors(chain = true)
 public class Verification
 {
 }
